1c1dcae1ec268ca7e28ab290001641988d427175
LeonamSilva <leonamsilva@ufpa.br>
2012-06-20 12:00:14 -0300

    Testando commit

--- mysite/twitter/models.py
+++ mysite/twitter/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 # Create your models here.
-
+# Table Comments
 class Comments(models.Model):
   date = models.DateField()
   author = models.CharField(max_length=30)
