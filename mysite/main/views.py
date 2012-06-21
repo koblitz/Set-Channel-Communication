61d67ef41a6c0ae9b53786ea8cd3c6b9f5dd96bb
LeonamSilva <leonamsilva@ufpa.br>
2012-06-21 11:32:54 -0300

    Quase Pronto

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 # Create your views here.
 from time import gmtime, strftime
+from mysite.twitter.models import Comments
 from django.shortcuts import render_to_response
 
 def twitter(request):
+  
   date = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
-  return render_to_response('mysite/src/html/template.html', {'date':date})
+  
+  comments = Comments.objects.all()
+  
+  comments.reverse()
+  
+  return render_to_response('mysite/src/html/template.html', {'date':date, 'list':comments})
 
+
+  
+  
--- mysite/twitter/views.py
+++ mysite/twitter/views.py
@@ -2,10 +2,14 @@
 # Create your views here.
 from models import Comments
 from django.http import HttpResponseRedirect
+from datetime import date
 
 def comment(request):
   #processamento
   comment = request.GET.get('text','')
-  c = Comments(date='',author='',text=comment)
+  if comment:
+    today = date.today()
+    c = Comments(date=today,author='',text=comment)
+    c.save()
   return HttpResponseRedirect('/twitter/')
   
