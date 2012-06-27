e97d7b28f08f88aaa3b2b800dd02e132d90abdb4
LeonamSilva <leonamsilva@ufpa.br>
2012-06-27 14:40:11 -0300

    esse modulo será uma interface para acessar models, e não processa mais requests como dantes

--- mysite/twitter/views.py
+++ mysite/twitter/views.py
@@ -2,16 +2,21 @@
 # Create your views here.
 from models import Comments
 from django.http import HttpResponseRedirect
-from datetime import date
 
-def comment(request):
+
+def comment(text,date):
   #processamento
-  comment = request.GET.get('text','')
-  if comment:
-    today = date.today()
-  if len(comment)>70:
-    return HttpResponseRedirect('/twitter/')
   c = Comments(date=today,author='',text=comment)
   c.save()
-  return HttpResponseRedirect('/twitter/')
+
+def retrieve():
+  comments = Comments.objects.all()
+  return comments.reverse()
+    	
+      		
+  
+  
+  
+  
+  
   
