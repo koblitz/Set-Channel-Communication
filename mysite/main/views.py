6782f0ab4edf0ccca1672311baa366bfca8898cd
LeonamSilva <leonamsilva@ufpa.br>
2012-07-02 09:01:33 -0300

    Acrescido metodo para responder a postar tweet

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,14 +2,19 @@
 # Create your views here.
 from datetime import date
 from mysite.twitter.views import comment,retrieve
+
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+
+_date = date.today()
 
 def twitter(request):
-  _date = date.today()
-  if request.is_ajax():
-    comment = request.GET.get('text','')	
-    if comment and comment<70:
-      comment(comment,data)
+  
   comments = retrieve()
   return render_to_response('mysite/html/index.html', {'date':_date, 'comments':comments})
-
+  
+def post_twitter(request,text):
+  if request.isAjax():
+    comment(text,_date)
+  #HttpResponseRedirect('/twitter/')  
+  
