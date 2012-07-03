8c5fcfa2ea299325dcae66f38bff072d3a8e31fa
LeonamSilva <set@set-HP-Compaq-8000-Elite-SFF-PC.(none)>
2012-07-03 12:03:10 -0300

    Mudei a ER para capturar a url post_twitter

--- mysite/urls.py
+++ mysite/urls.py
@@ -11,7 +11,7 @@ from mysite.main.views import twitter,post_twitter
 
 urlpatterns = patterns('',
     (r'^twitter/$', twitter),
-    (r'^twitter/post_twitter/(?P<text>\w+)/$',post_twitter),
+    (r'^twitter/post_twitter/?text=([a-zA-Z0-9+]+)$',post_twitter),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
@@ -20,5 +20,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
