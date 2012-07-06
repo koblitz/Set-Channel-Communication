1e914033419380bcbe637055f194cb3e8ccca979
LeonamSilva <set@set-HP-Compaq-8000-Elite-SFF-PC.(none)>
2012-07-06 12:56:22 -0300

    tudo comentado

--- mysite/html/index.js
+++ mysite/html/index.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-	$('#comment_submit').click(function() {
-		q = $('#comment_textarea').val();
-			if (q.length > 70)
-				alert('Seu Tweet precisa ter menos que 70 caracteres!');	
-			else
-				  $.ajax({
-					    type: "GET",
-                                           url:"post_twitter/",
-					    data:{
-                                                   'text':q,
-                                                },
-                                           success: function(data){
-                                                       $('body').html(data);
-                                                          }
-                                        });
-	})
- });
-
-
+// $(document).ready(function(){
+// 	$('#comment_submit').click(function() {
+// 		q = $('#comment_textarea').val();
+// 			if (q.length > 70)
+// 				alert('Seu Tweet precisa ter menos que 70 caracteres!');	
+// 			else
+// 				  $.ajax({
+// 					    type: "GET",
+//                                            url:"post_twitter/",
+// 					    data:{
+//                                                    'text':q,
+//                                                 },
+//                                            success: function(data){
+//                                                        $('body').html(data);
+//                                                           }
+//                                         });
+// 	})
+//  });
+// 
+// 
