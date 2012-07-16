f24838e95b2b90c97d91de4d1328f1d13070c954
LeonamSilva <leonamsilva@ufpa.br>
2012-07-16 13:26:53 -0300

    16/07/12

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,26 +2,15 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
 
-from mysite.main.views import twitter,post_twitter,login,logout
-
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^twitter/$', twitter),
-    (r'^twitter/post_twitter/$',post_twitter),
-    (r'^twitter/login/$',login),
-    (r'^twitter/logout/$',logout),
-    #(r'^admin/', include('django.contrib.admin.urls')),
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('mysite.main.views',
+    (r'^twitter/$','twitter'),
+    (r'^twitter/post_twitter/$','post_twitter'),
+    (r'^twitter/login/$','login'),
+    (r'^twitter/logout/$','logout'),
+    (r'^twitter/register/$','register'),
 )
