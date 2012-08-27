6d099254ba84e7e14ebf7fb3a15cd1774262a496
LeonamSilva <leonamsilva@ufpa.br>
2012-08-27 12:04:30 -0300

    Troquei Context por RequestContext

--- apps/main/ajax.py
+++ apps/main/ajax.py
@@ -1,19 +1,19 @@
 
 from apps.twitter.views import comment,retrieve
 from django.template.loader import render_to_string
-from django.template import Context
+from django.template import RequestContext
 
 from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
 
-@dajaxice_register
+@dajaxice_register(method='GET', name='ajax.comment')
 def post_twitter(request):
-  print "ois"
+  
   text = request.GET['text']
   if text and len(text)<=70:
     comment(text,request.user.username,_date)
     comments = retrieve()
-    refresh = render_to_string('index.html',Context({'date':_date, 'comments':comments, 'user':request.user.username}))
+    refresh = render_to_string('index.html',RequestContext({'date':_date, 'comments':comments, 'user':request.user.username}))
     
     dajax = Dajax()
     dajax.assign('body','innerHTML',refresh)
