f9132ccac5f72a01585ec15e7cca261cbfa74ad6
LeonamSilva <leonamsilva@ufpa.br>
2012-08-27 12:05:18 -0300

    Retirei url para post_twitter agora desnecessária

--- conf/urls.py
+++ conf/urls.py
@@ -6,6 +6,7 @@ import settings
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
 dajaxice_autodiscover()
 
 
@@ -15,7 +16,6 @@ dajaxice_autodiscover()
 
 urlpatterns = patterns('apps.main.views',
     (r'^twitter/$','twitter'),
-    #(r'^twitter/post_twitter/$','post_twitter'),
     (r'^twitter/login/$','login'),
     (r'^twitter/logout/$','logout'),
     (r'^twitter/register/$','register'),
